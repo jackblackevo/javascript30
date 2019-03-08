602f85a8dd05b2a2168eed295a0cb43b458a4f6c
ryan.c <jackblackevo@gmail.com>
2019-03-09 01:41:17 +0800

    fix(02_css_and_js_clock): clock * Fix second hand lose transition effect

--- 02_css_and_js_clock/clock.mjs
+++ 02_css_and_js_clock/clock.mjs
@@ -4,36 +4,39 @@ const hourHand = document.querySelector('.hour-hand');
 const minHand = document.querySelector('.min-hand');
 const secondHand = document.querySelector('.second-hand');
 
-let transformStyle = '';
 const updateHandPosition = (handElement, degreeDiff) => {
-  if (handElement.style.transition === 'unset') {
-    handElement.style.transition = transformStyle;
-  }
-  const nextDegree = 90 + degreeDiff;
-  if (degreeDiff === 0) {
-    handElement.style.transform = `translateY(-50%)
-    rotate(${nextDegree + 359}deg)`;
-    transformStyle = getComputedStyle(handElement).transition;
+  if (handElement.dataset.degree === '90') {
+    const { transition: transitionStyle } = getComputedStyle(handElement);
     handElement.style.transition = 'unset';
+    handElement.style.transform = 'translateY(-50%) rotate(90deg)';
+    void handElement.offsetTop;
+    handElement.style.transition = transitionStyle;
   }
-  handElement.style.transform = `translateY(-50%) rotate(${nextDegree}deg)`;
+
+  const nextDegree = 90 + degreeDiff;
+  handElement.dataset.degree = nextDegree;
+  handElement.style.transform =
+    nextDegree === 90
+      ? `translateY(-50%) rotate(${nextDegree + 360}deg)`
+      : (handElement.style.transform = `translateY(-50%) rotate(${nextDegree}deg)`);
 };
 
-let animationID = 0;
-const updateClock = ({ hour, min, second }) => {
+let timerID = 0;
+const updateClock = () => {
+  const { hour, min, second } = getTimes();
   updateHandPosition(hourHand, (hour / 12) * 360);
   updateHandPosition(minHand, (min / 60) * 360);
   updateHandPosition(secondHand, (second / 60) * 360);
-
-  animationID = requestAnimationFrame(() => updateClock(getTimes()));
 };
 
 const startClock = () => {
-  animationID = requestAnimationFrame(() => updateClock(getTimes()));
+  stopClock();
+  timerID = setInterval(updateClock, 1000);
 };
 
 const stopClock = () => {
-  cancelAnimationFrame(animationID);
+  clearInterval(timerID);
+  timerID = 0;
 };
 
 export { startClock, stopClock };
