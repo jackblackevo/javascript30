7bf06cee4c7235a49932eda454505cc6c35ecd5e
ryan.c <jackblackevo@gmail.com>
2019-03-10 00:36:00 +0800

    refactor(02_css_and_js_clock): clock * Make updateHandPosition to handle more sisituations * Update Clock Immediately on start

--- 02_css_and_js_clock/clock.mjs
+++ 02_css_and_js_clock/clock.mjs
@@ -5,25 +5,28 @@ const minHand = document.querySelector('.min-hand');
 const secondHand = document.querySelector('.second-hand');
 
 const updateHandPosition = (handElement, degreeDiff) => {
-  if (handElement.dataset.degree === '90') {
+  const lastDegree = Number.parseInt(handElement.dataset.degree || 90, 10);
+  const nextDegree = 90 + degreeDiff;
+
+  const isOver12 = lastDegree >= 450;
+  if (isOver12) {
     const { transition: transitionStyle } = getComputedStyle(handElement);
     handElement.style.setProperty('transition', 'unset');
     handElement.style.setProperty(
       'transform',
-      'translateY(-50%) rotate(90deg)'
+      `translateY(-50%) rotate(${lastDegree - 360}deg)`
     );
     void handElement.offsetTop;
     handElement.style.setProperty('transition', transitionStyle);
   }
 
-  const nextDegree = 90 + degreeDiff;
-  handElement.dataset.degree = nextDegree;
+  const degree =
+    !isOver12 && nextDegree < lastDegree ? nextDegree + 360 : nextDegree;
   handElement.style.setProperty(
     'transform',
-    nextDegree === 90
-      ? `translateY(-50%) rotate(${nextDegree + 360}deg)`
-      : `translateY(-50%) rotate(${nextDegree}deg)`
+    `translateY(-50%) rotate(${degree}deg)`
   );
+  handElement.dataset.degree = degree;
 };
 
 const updateClock = () => {
@@ -36,6 +39,7 @@ const updateClock = () => {
 let timerID = 0;
 const startClock = () => {
   stopClock();
+  updateClock();
   timerID = setInterval(updateClock, 1000);
 };
 
